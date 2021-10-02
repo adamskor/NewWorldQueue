c9e970a6f080713c4c2db356e883b1fb825f32b5
adamskor <74060648+adamskor@users.noreply.github.com>
2021-10-02 12:03:57 +0200

    Add files via upload

---
+++ NewWorld.py
@@ -0,0 +1,37 @@
+import time as t
+from "/pyautogui" import setup
+
+def main():
+    """Values that you might want to change"""
+    ScreenResolutionWidth = 1920
+    ScreenResolutionHeight = 1080
+
+    """Values that you will want to change"""
+    "Time until you join the queue:"
+    Hours = 4
+    Minutes = 30
+
+    """Values you should not touch"""
+    WidthScaling = 1678/1920
+    HeightScaling = 957/1080
+    xPixel = ScreenResolutionWidth*WidthScaling
+    yPixel = ScreenResolutionHeight*HeightScaling
+    sleepTime = Hours*3600 + Minutes*60
+
+    """Logic you should not touch"""
+    t.sleep(sleepTime)
+    for i in range(3):
+        pg.moveTo(ScreenResolutionWidth/2, ScreenResolutionHeight/2)
+        pg.click()
+    t.sleep(1)
+    for i in range(3):
+        t.sleep(1)
+        pg.moveTo(xPixel, yPixel)
+        pg.click()
+    t.sleep(1)
+    for i in range(3):
+        pg.moveTo(ScreenResolutionWidth - 20, ScreenResolutionHeight - 20)
+        pg.click()
+
+if __name__ == "__main__":
+    main()
